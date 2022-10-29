b3b7f34d49a847df02ce600cc5300fac3664144e
Duong PY <113834956+duongvodich@users.noreply.github.com>
2022-10-29 16:53:34 +0700

    Update and rename rac.py to frame_traodoisub_Tiktok.py

--- frame_traodoisub_Tiktok.py
+++ frame_traodoisub_Tiktok.py
@@ -1,15 +1,17 @@
 from codecs import unicode_escape_decode
 from unicodedata import unidata_version
+#import session để lưu lại cookie >> nhớ không lầm là vậy
 from requests import session
 
-uid_tiktok= 'duongthanhtuyen20'
-token_tds = '&access_token=TDS0nIwEjclZXZzJiOiIXZ2V2ciwiI5Ijbp12Zu92YiojIyV2c1Jye'
+uid_tiktok= 'uid_Tiktok'
+token_tds = '&access_token={Token_traodoisub}'
 
 url_cauhinh = 'https://traodoisub.com/api/?fields=tiktok_run&id='+uid_tiktok+token_tds
 url_nhiemvu = 'https://traodoisub.com/api/?fields=tiktok_like&access_token='+token_tds
 
 all_urlsetting = url_cauhinh
 all_url_like = url_nhiemvu + token_tds
+
 s = session()
 def setting_cau_hinh():
     url_get = s.get(all_urlsetting)
